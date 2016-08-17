6c4887ee0b0c97300f4499ef8d9c88a3da50aecf
min-ima <Irene@Irenes-MBP.fritz.box>
2016-08-17 22:09:12 +0200

    KWIC function

--- menu.js
+++ menu.js
@@ -1,5 +1,25 @@
 
-var puncts = ["!",".","?"];
+// var puncts = ["!",".","?"];
+
+function getClosestInt(givenInt, intList, pos) {
+	/*Find the interger in intList closest to givenInt either before or 
+	after depending on pos (True = before) */
+	if (intList[0] >= givenInt) {
+		return -2;
+	}
+
+	var i = 0;
+	while (intList[i] < givenInt) {
+			i++;
+		}
+
+	if (pos) {
+		return intList[i-1];
+	} else {
+		return intList [i];
+	}
+}
+
 
 function findOccur(toSearchIn,toFind) {
 	/*Find all occurences of string toFind in string to SarchIn*/
@@ -13,75 +33,65 @@ function findOccur(toSearchIn,toFind) {
 	return finds;
 }
 
+
 function createKWICDiv(toSearchIn, finds) {
-	/*Create a new Div container for given keyword*/
-	var divLeft = document.createElement("div");
-	divLeft.className = "divLeft";
-	var divMiddle = document.createElement("div");
-	divMiddle.className = "divMiddle";
-	divMiddle.addEventListener("click", function() {
-           window.location = "visual.html";
-});
-	var divRight = document.createElement("div");
-	divRight.className = "divRight";
+	/*Create divs for given keyword*/
 
+	//find visual linguistics
+	
+	var divleft = document.createElement("div");
+	divleft.className = "divLeft";
+	var divmiddle = document.createElement("div");
+	divmiddle.className = "divMiddle";
+	divmiddle.addEventListener("click", function() {
+	    window.location = "visual.html";
+	});
+	var divright = document.createElement("div");
+	divright.className = "divRight";
 
+	var puncts = findOccur(toSearchIn,"."); //to stop at puncts  todo: foreach in puncts array find occur and add
+
+	//enter text in divs
+	finds.forEach(function(index,i) {
+		//prefix
+		var prefix = document.createElement("p");
+		if (toSearchIn[index-2] == "." || toSearchIn[index-2] == "!"){
+			prefix.textContent = "...";
+		}
+		else {
+			var punctBefore = getClosestInt(index, puncts, true);
+			prefix.textContent = toSearchIn.substring(punctBefore+2,index);	
+		}
+		divleft.appendChild(prefix);
+
+		//middle
+		var middle = document.createElement("p")
+		middle.textContent = toFind;
+		divmiddle.appendChild(middle);
+
+		//postfix
+		var postfix = document.createElement("p")
+		if (toSearchIn[index+toFind.length] == ".") {
+			postfix.textContent = "."
+		} else {
+			var punctAfter = getClosestInt(index+toFind.length-1,puncts,false);
+			postfix.textContent = toSearchIn.substring(index+toFind.length, punctAfter+length+1);
+		}
+		divright.appendChild(postfix);
+	});
+
+	//create master div for easier CSS handling
+	var master = document.createElement("div");
+	master.className = "divMaster";
+	master.appendChild(divleft);
+	master.appendChild(divmiddle);
+	master.appendChild(divright);
+	document.body.appendChild(master);
 }
 
 
-//find visual linguistics
-var visual = "This is a test for Visual Linguistics. The project Visual Linguistics is great. This is a sentence without the words I am looking for. Visual Linguistics is led by Noah Bubenhofer. And this is an empty one too. In Visual Linguistics we have fun."
-var divleft = document.createElement("div");
-divleft.className = "divLeft";
-var divmiddle = document.createElement("div");
-divmiddle.className = "divMiddle";
-divmiddle.addEventListener("click", function() {
-    window.location = "visual.html";
-});
-var divright = document.createElement("div");
-divright.className = "divRight";
-
 var toFind = "Visual Linguistics";
-
-var finds = findOccur(visual,toFind);
-var puncts = findOccur(visual,"."); //to stop at puncts
-
-//enter text in divs
-finds.forEach(function(index,i) {
-	//prefix
-	var prefix = document.createElement("p");
-	if (visual[index-2] == "." || visual[index-2] == "!"){
-		prefix.textContent = ""
-	}
-	else {
-		prefix.textContent = visual.substring(index-20,index);	
-	}
-	divleft.appendChild(prefix);
-
-	//middle
-	var middle = document.createElement("p")
-	middle.textContent = toFind;
-	divmiddle.appendChild(middle);
-
-	//postfix
-	var postfix = document.createElement("p")
-	if (visual[index+toFind.length] == ".") {
-		postfix.textContent = "."
-	} else {
-		postfix.textContent = visual.substring(index+toFind.length, index+toFind.length+20);
-	}
-	divright.appendChild(postfix);
-});
-var master = document.createElement("div");
-master.className = "divMaster";
-master.appendChild(divleft);
-master.appendChild(divmiddle);
-master.appendChild(divright);
-document.body.appendChild(master);
-
-
-// document.body.appendChild(divleft);
-// document.body.appendChild(divmiddle);
-// document.body.appendChild(divright);
-
+var toSearchIn = "This is a test for Visual Linguistics. The project Visual Linguistics is great. This is a sentence without the words I am looking for. Visual Linguistics is led by Noah Bubenhofer. And this is an empty one too. In Visual Linguistics we have fun."
+var finds = findOccur(toSearchIn,toFind);
+createKWICDiv(toSearchIn,finds);
 
